03fa2d9b8f908374e18f7872884ad2fe14f2bab7
poper0 <sjmeyer98@gmail.com>
2016-08-13 19:29:12 -0400

    a

--- Foldertree.php
+++ Foldertree.php
@@ -7,6 +7,20 @@
 
 */
 
+@WebFilter(urlPatterns="/rest/*")
+public class AllowAccessFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest sRequest, ServletResponse sResponse, FilterChain chain) throws IOException, ServletException {
+        System.out.println("in AllowAccessFilter.doFilter");
+        HttpServletRequest request = (HttpServletRequest)sRequest;
+        HttpServletResponse response = (HttpServletResponse)sResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type"); 
+        chain.doFilter(request, response);
+    }
+}
+
 class treeview {
 
 	private $files;
